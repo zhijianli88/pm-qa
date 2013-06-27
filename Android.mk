0eff2f142fb36e73e892c13020e9bdc21e1517f9
Amit Pundir <amit.pundir@linaro.org>
2013-06-27 18:24:22 +0530

    pm-qa: for android update build tag to eng
    - android builds are by default eng type. It helps build system to
      copy binaries to out-dir properly.
    - bug: https://bugs.launchpad.net/linaro-android/+bug/1125984
    
    Signed-off-by: Sanjay Singh Rawat <sanjay.rawat@linaro.org>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(call all-subdir-makefiles)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := tests
 LOCAL_MODULE := Switches.sh
 LOCAL_SRC_FILES := ../../external/pm-qa/Switches.sh
